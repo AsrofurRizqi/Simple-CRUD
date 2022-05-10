e9e081c5b31849e066fda13870f6b0b6c4d0537c
AsrofurRizqi <57488230+AsrofurRizqi@users.noreply.github.com>
2022-05-10 09:23:33 +0700

    Update connection.php

--- sys/connection.php
+++ sys/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string = "host=ec2-3-217-251-77.compute-1.amazonaws.com port=5432 dbname=d4tpr606kr69nu user=gjbtjnzueraetk password=499fe98c355e60089883ce1b1a8ee3300ca57dd45b5ed4a2ede7f2141715dc9f";
+$conn_string = "hostdb.com port=5432 dbname=xxxxxxx user=xxxxx password=xxxx";
 $conn = pg_connect($conn_string);
 $conn
 // if($conn) {
